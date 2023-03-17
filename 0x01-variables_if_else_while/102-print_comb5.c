f0189df1f5d08bcddc5ecad715d2a9ae5c98357d
flifalmed <flifalmed@gmail.com>
2023-03-17 06:09:05 -0700

    task-12

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints all possible combinations of two two-digit numbers
+ * ranging from 0 to 99, separated by a space and comma.
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i, j;
+
+	for (i = 0; i <= 99; i++)
+	{
+		for (j = i; j <= 99; j++)
+		{
+			int first_digit_i = i / 10;
+			int second_digit_i = i % 10;
+			int first_digit_j = j / 10;
+			int second_digit_j = j % 10;
+
+			putchar('0' + first_digit_i);
+			putchar('0' + second_digit_i);
+			putchar(' ');
+			putchar('0' + first_digit_j);
+			putchar('0' + second_digit_j);
+			if (!(i == 99 && j == 99))
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	return (0);
+}
+
