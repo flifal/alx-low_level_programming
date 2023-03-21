b63235963a3c40b352b81575ac4c0d79abacdf07
flifalmed <flifalmed@gmail.com>
2023-03-21 04:15:28 -0700

    task-4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _isalpha - function to sprcify if prined letter or not
+ *
+ * @c: printed
+ * Return: 1 if c is letter and 0 otherwise
+ */
+int _isalpha(int c)
+{
+	if (c <= 90 && c >= 65 && c >= 97 && c <= 122)
+		return (1);
+	else
+		return (0);
+}
