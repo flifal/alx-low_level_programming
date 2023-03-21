8c9b58cfd1ae46b1b703d9f93c7d5627d3a59c77
flifalmed <flifalmed@gmail.com>
2023-03-21 07:51:15 -0700

    task-0

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * positive_or_negative - check for prositive or negative
+ * @i: parameter
+ * Return: 0
+ */
+void positive_or_negative(int i)
+{
+	if (i < 0)
+		printf("%d is negative", i);
+	else
+		printf("%d is positive", i);
+}
