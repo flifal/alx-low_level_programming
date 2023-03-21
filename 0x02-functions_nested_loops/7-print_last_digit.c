60c0b26802dcbb07f1bb3ad053eace4dcc1f9bec
flifalmed <flifalmed@gmail.com>
2023-03-21 04:58:17 -0700

    task-7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_last_digit - function to print last digit
+ * @i: function parameter
+ * Return: j
+ */
+int print_last_digit(int i)
+{
+	int j;
+
+	j = i % 10;
+	if (i < 0)
+		j = -j;
+	return (j);
+}
