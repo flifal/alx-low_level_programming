7b6b240c3fadb81e6265a3c145e72cac62cd5919
flifalmed <flifalmed@gmail.com>
2023-03-21 05:52:57 -0700

    task-10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - addiotonal two values
+ * @n1: the num one
+ * @n2: the num two
+ * Return: integer value
+ */
+
+int add(int n1, int n2)
+{
+	return (n1 + n2);
+}
