2877bef470c7e18760301977a0ac2a9fb7f455ba
flifalmed <flifalmed@gmail.com>
2023-04-03 07:22:01 -0700

    0x07

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,5 +1,5 @@
 #include "main.h"
-#include <stddef.h>
+#include <stdio.h>
 
 /**
  * _strpbrk - searches a string for any of a set of bytes
@@ -20,9 +20,7 @@ char *_strpbrk(char *s, char *accept)
 			for (j = 0; accept[j] != '\0'; j++)
 			{
 				if (s[i] == accept[j])
-				{
 					return (s + i);
-				}
 			}
 		}
 	}
