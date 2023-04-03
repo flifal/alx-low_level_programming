bdc3f1fc4a8dbd4b90752839d4582dde1cfcaf6e
flifalmed <flifalmed@gmail.com>
2023-04-03 06:41:07 -0700

    0x07

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strchr - locates a character is a string
+ * @s: pointer to a string
+ * @c: character to locate
+ *
+ * Return: pointer to first accurence of the character c in string s, Null, if the character is not found
+ */
+char *_strchr(char *s, char c)
+{
+	for (; *s != '\0'; s++)
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+	}
+	if (c == '\0')
+	{
+		return (s);
+	}
+	return (0);
+}
